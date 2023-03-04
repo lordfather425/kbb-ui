a6bbf27aa415b17b0b3daf9bbb79f1739e670828
Greg <51519968+lordfather425@users.noreply.github.com>
2023-03-04 18:09:23 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,32 +1,36 @@
-// Send a message to the background script to retrieve the Kelly Blue Book value
-chrome.runtime.sendMessage({action: "getKBBValue", title: document.title}, function(response) {
-  if (chrome.runtime.lastError) {
-    // Display an error message if there was an error communicating with the background script
-    console.error("Error communicating with the background script:", chrome.runtime.lastError);
-    return;
-  }
+function addValueOverlay(value) {
+  const overlay = document.createElement("div");
+  overlay.style.position = "fixed";
+  overlay.style.top = "0";
+  overlay.style.left = "0";
+  overlay.style.width = "100%";
+  overlay.style.height = "100%";
+  overlay.style.background = "rgba(0, 0, 0, 0.5)";
+  overlay.style.zIndex = "9999";
+  overlay.style.display = "flex";
+  overlay.style.alignItems = "center";
+  overlay.style.justifyContent = "center";
 
+  const valueDisplay = document.createElement("div");
+  valueDisplay.style.color = "#fff";
+  valueDisplay.style.fontSize = "2em";
+  valueDisplay.style.textAlign = "center";
+  valueDisplay.style.padding = "1em";
+  valueDisplay.textContent = `Estimated value: $${value}`;
+
+  overlay.appendChild(valueDisplay);
+  document.body.appendChild(overlay);
+}
+
+function getTitle() {
+  const title = document.querySelector("h2.postingtitle span.postingtitletext:first-of-type").textContent.trim();
+  return title.split(" - ")[0];
+}
+
+chrome.runtime.sendMessage({ action: "getCarValue", title: getTitle() }, function(response) {
   if (response.error) {
-    // Display an error message if the background script encountered an error retrieving the KBB value
-    console.error("Error retrieving KBB value:", response.error);
-    return;
+    console.error(response.error);
+  } else {
+    addValueOverlay(response.value);
   }
-
-  // Create a div element to hold the value
-  var kbbValueDiv = document.createElement("div");
-  
-  // Set the div's styles
-  kbbValueDiv.style.position = "fixed";
-  kbbValueDiv.style.bottom = "10px";
-  kbbValueDiv.style.right = "10px";
-  kbbValueDiv.style.padding = "10px";
-  kbbValueDiv.style.backgroundColor = "#f9f9f9";
-  kbbValueDiv.style.border = "1px solid #ccc";
-  
-  // Create a text node with the value and append it to the div
-  var kbbValueTextNode = document.createTextNode("Kelly Blue Book Value: $" + response.value);
-  kbbValueDiv.appendChild(kbbValueTextNode);
-  
-  // Append the div to the document body
-  document.body.appendChild(kbbValueDiv);
 });
